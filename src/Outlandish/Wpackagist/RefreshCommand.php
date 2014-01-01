407575ed98cbb41a5cabd293264be71bbf4f42c3
Steve Buzonas <steve@fancyguy.com>
2014-01-01 02:50:01 +0000

    added ability to update entries in database for core, themes, and plugins as well as the ability to fetch the versions for them utilizing the composer version parser

--- src/Outlandish/Wpackagist/RefreshCommand.php
+++ src/Outlandish/Wpackagist/RefreshCommand.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Outlandish\Wpackagist;
-
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,64 +8,114 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RefreshCommand extends Command {
+
 	protected function configure() {
 		$this
-				->setName('refresh')
-				->setDescription('Refresh list of plugins from WP SVN')
-				->addOption(
-					'svn',
-					null,
-					InputOption::VALUE_REQUIRED,
-					'Path to svn executable',
-					'svn'
-				)->addOption(
-					'base',
-					null,
-					InputOption::VALUE_REQUIRED,
-					'Subversion repository base',
-					'http://plugins.svn.wordpress.org/'
-				);
+			->setName('refresh')
+			->setDescription('Refresh list of plugins and themes from WP SVN')
+			->addOption(
+				'svn', null, InputOption::VALUE_REQUIRED, 'Path to svn executable', 'svn'
+			)->addOption(
+			'base', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress core', 'http://core.svn.wordpress.org/'
+		)->addOption(
+			'pluginbase', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress plugins', 'http://plugins.svn.wordpress.org/'
+		)->addOption(
+			'themebase', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress themes', 'http://themes.svn.wordpress.org/'
+		);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$base = rtrim($input->getOption('base'), '/').'/';
-		$svn = $input->getOption('svn');
+		$svn_path = $input->getOption('svn');
+
+		$base = rtrim($input->getOption('base'), '/') . '/';
+		$output->writeln("Fetching WordPress svn info from $base");
+		$this->updateCore($svn_path, $base);
+
+		$plugin_base = rtrim($input->getOption('pluginbase'), '/') . '/';
+		$this->refreshPackages($output, $svn_path, $plugin_base, 'wordpress-plugin');
+
+		$theme_base = rtrim($input->getOption('themebase'), '/') . '/';
+		$this->refreshPackages($output, $svn_path, $theme_base, 'wordpress-theme');
+	}
+
+	private function getPackages($svn_path, $svn_base) {
+		exec("$svn_path ls --xml $svn_base", $xmlLines, $returnCode);
+		if ($returnCode) {
+			throw new Exception("Error from svn command", $returnCode);
+		}
+		$xml = simplexml_load_string(implode("\n", $xmlLines));
+
+		return $xml;
+	}
+
+	private function updateCore($svn_path, $svn_base, $pkg_name = "wordpress", $pkg_group = "wordpress") {
+		exec("$svn_path info --xml $svn_base", $xmlLines, $returnCode);
+		if ($returnCode) {
+			throw new Exception("Error from svn command", $returnCode);
+		}
+		$xml = simplexml_load_string(implode("\n", $xmlLines));
+		unset($xmlLines);
 
 		/**
 		 * @var \PDO $db
 		 */
 		$db = $this->getApplication()->getDb();
 
-		$output->writeln("Fetching full plugin list from $base");
+		$updateStmt = $db->prepare('UPDATE packages SET last_committed = :date WHERE name = :name AND type = :type');
 
-		exec("$svn ls --xml $base 2>&1", $xmlLines, $returnCode);
-		if ($returnCode) {
-			$output->writeln('<error>Error from svn command</error>');
-			return 1; //error code
-		}
-		$xml = simplexml_load_string(implode("\n", $xmlLines));
-
-		$output->writeln("Updating database");
-
-		$updateStmt = $db->prepare('UPDATE plugins SET last_committed = :date WHERE name = :name');
-		$insertStmt = $db->prepare('INSERT INTO plugins (name, last_committed) VALUES (:name, :date)');
 		$db->beginTransaction();
-		$newCount = 0;
-		foreach ($xml->list->entry as $entry) {
-			$date = date('Y-m-d H:i:s', strtotime((string)$entry->commit->date));
-			$params = array(':name' => (string)$entry->name, ':date' => $date);
+		$date = date('Y-m-d H:i:s', strtotime((string) $xml->entry->commit->date));
+		$params = array(':name' => (string) $pkg_name, ':type' => (string) $pkg_group, ':date' => $date);
+		$updateStmt->execute($params);
+		if ($updateStmt->rowCount() == 0) {
+			$insertStmt = $db->prepare('INSERT INTO packages (name, type, last_committed) VALUES (:name, :type, :date)');
+			$insertStmt->execute($params);
+		}
+		$db->commit();
+	}
+
+	private function updateDatabase(\SimpleXMLElement $packages, $pkg_group) {
+		/**
+		 * @var \PDO $db
+		 */
+		$db = $this->getApplication()->getDb();
+
+		$updateStmt = $db->prepare('UPDATE packages SET last_committed = :date WHERE name = :name AND type = :type');
+		$insertStmt = $db->prepare('INSERT INTO packages (name, type, last_committed) VALUES (:name, :type, :date)');
+		$db->beginTransaction();
+		$newPackages = 0;
+		foreach ($packages->list->entry as $entry) {
+			$date = date('Y-m-d H:i:s', strtotime((string) $entry->commit->date));
+			$params = array(':name' => (string) $entry->name, ':type' => (string) $pkg_group, ':date' => $date);
 
 			$updateStmt->execute($params);
 			if ($updateStmt->rowCount() == 0) {
 				$insertStmt->execute($params);
-				$newCount++;
+				$newPackages++;
 			}
 		}
 		$db->commit();
 
-		$updateCount = $db->query('SELECT COUNT(*) FROM plugins WHERE last_fetched < last_committed')->fetchColumn();
+		$updatedPackages = $db->query('SELECT COUNT(*) FROM packages WHERE last_fetched < last_committed')->fetchColumn();
 
-		$output->writeln("Found $newCount new and $updateCount updated plugins");
+		return array($newPackages, $updatedPackages);
+	}
+
+	private function refreshPackages(OutputInterface $output, $svn_path, $svn_base, $pkg_group) {
+		$output->writeln("Fetching full $pkg_group list from $svn_base");
+
+		try {
+			$packages = $this->getPackages($svn_path, $svn_base);
+		} catch (Exception $e) {
+			$output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+			return 1; // error code
+		}
+
+		$output->writeln("Updating database");
+
+		list($newPackages, $updatedPackages) = $this->updateDatabase($packages, $pkg_group);
+
+		$output->writeln(sprintf("Found %s new and %s updated %s packages", $newPackages, $updatedPackages, $pkg_group));
 	}
 
 }
--- src/Outlandish/Wpackagist/UpdateCommand.php
+++ src/Outlandish/Wpackagist/UpdateCommand.php
@@ -1,71 +1,166 @@
 <?php
 
-
 namespace Outlandish\Wpackagist;
 
-
+use Composer\Package\Version\VersionParser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateCommand extends Command
-{
+class UpdateCommand extends Command {
+
+	private $versionParser;
+
 	protected function configure() {
 		$this
-				->setName('update')
-				->setDescription('Update version info for individual plugins')
-				->addOption(
-					'svn',
-					null,
-					InputOption::VALUE_REQUIRED,
-					'Path to svn executable',
-					'svn'
-				)->addOption(
-					'base',
-					null,
-					InputOption::VALUE_REQUIRED,
-					'Subversion repository base',
-					'http://plugins.svn.wordpress.org/'
-				);
+			->setName('update')
+			->setDescription('Update version info for individual plugins')
+			->addOption(
+				'svn', null, InputOption::VALUE_REQUIRED, 'Path to svn executable', 'svn'
+			)->addOption(
+			'base', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress core', 'http://core.svn.wordpress.org/'
+		)->addOption(
+			'pluginbase', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress plugins', 'http://plugins.svn.wordpress.org/'
+		)->addOption(
+			'themebase', null, InputOption::VALUE_REQUIRED, 'Subversion repository base for WordPress themes', 'http://themes.svn.wordpress.org/'
+		);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$base = rtrim($input->getOption('base'), '/') . '/';
-		$svn = $input->getOption('svn');
+		$svn_path = $input->getOption('svn');
+		$package_types = array(
+			'wordpress' => array(
+				'svn_base' => rtrim($input->getOption('base'), '/') . '/',
+				'is_core' => true
+			),
+			'wordpress-plugin' => array(
+				'svn_base' => rtrim($input->getOption('pluginbase'), '/') . '/',
+				'is_core' => false
+			),
+			'wordpress-theme' => array(
+				'svn_base' => rtrim($input->getOption('themebase'), '/') . '/',
+				'is_core' => false
+			)
+		);
 
+		$this->versionParser = new VersionParser;
+
+		foreach ($package_types as $pkg_group => $fetch_options) {
+			$this->updatePackages($output, $svn_path, $fetch_options, $pkg_group);
+		}
+	}
+
+	private function updatePackages(OutputInterface $output, $svn_path, $fetch_options, $pkg_group) {
+		$packages = $this->getStalePackages($pkg_group);
+		foreach ($packages as $index => $package) {
+			$percent = floor($index / count($packages) * 1000) / 10;
+			$output->writeln(sprintf("<info>%04.1f%%</info> Fetching %s/%s", $percent, $package->type, $package->name));
+			try {
+				$this->fetchPackage($package, $svn_path, $fetch_options);
+			} catch (\Exception $e) {
+				$output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+				sleep(1);
+				continue;
+			}
+		}
+	}
+
+	private function getStalePackages($pkg_group) {
 		/**
 		 * @var \PDO $db
 		 */
 		$db = $this->getApplication()->getDb();
 
-		$plugins = $db->query('
-			SELECT * FROM plugins
-			WHERE last_fetched IS NULL OR last_fetched < last_committed
+		$stmt = $db->prepare('
+			SELECT * FROM packages
+			WHERE (last_fetched IS NULL OR last_fetched < last_committed) AND type = :type
 			ORDER BY last_committed DESC
-		')->fetchAll(\PDO::FETCH_OBJ);
+		');
+		$stmt->execute(array(':type' => $pkg_group));
+		$packages = $stmt->fetchAll(\PDO::FETCH_OBJ);
 
-		foreach ($plugins as $index => $plugin) {
-			$percent = floor($index / count($plugins) * 1000) / 10;
-			$output->writeln(sprintf("<info>%04.1f%%</info> Fetching %s", $percent, $plugin->name));
+		return $packages;
+	}
 
-			unset($tags);
-			exec("$svn ls $base{$plugin->name}/tags 2>&1", $tags, $returnCode);
-			if ($returnCode) {
-				$output->writeln('<error>Error from svn command</error>');
-				sleep(1); //there was an error so wait a bit and skip this iteration
-				continue;
-			}
-			$tags[] = 'trunk/';
-			$tags = array_map(function ($tag) {
-				return substr($tag, 0, -1);
-			}, $tags);
+	private function fetchPackage($package, $svn_path, $fetch_options) {
+		/**
+		 * @var \PDO $db
+		 */
+		$db = $this->getApplication()->getDb();
 
-			$stmt = $db->prepare('UPDATE plugins SET last_fetched = datetime("now"), versions = :json WHERE name = :name');
-			$plugin->versions = json_encode($tags);
-			$stmt->execute(array(':name' => $plugin->name, ':json' => $plugin->versions));
+		$svn_url = $fetch_options['is_core'] ? "{$fetch_options['svn_base']}tags" : "{$fetch_options['svn_base']}{$package->name}/tags";
+
+		exec("$svn_path ls $svn_url", $tags, $returnCode);
+		exec("$svn_path ls $svn_url", $branches, $returnCode);
+		if ($returnCode) {
+			throw new Exception("Error from svn command", $returnCode);
 		}
 
+		$stripRSlash = function($str) {
+			return rtrim($str, '/');
+		};
+
+		$tags = array_map($stripRSlash, $tags);
+		$branches = array_map($stripRSlash, $branches);
+
+		foreach ($tags as $tag) {
+			if (!$parsedTag = $this->validateTag($tag)) {
+				continue;
+			}
+
+			$data['version'] = $tag;
+			$data['version_normalized'] = $parsedTag;
+
+			$data['version'] = preg_replace('{[.-]?dev$}i', '', $data['version']);
+			$data['version_normalized'] = preg_replace('{(^dev-|[.-]dev$)}i', '', $data['version_normalized']);
+
+			if ($data['version_normalized'] !== $parsedTag) {
+				continue;
+			}
+			$versions[] = $data;
+		}
+
+		foreach ($branches as $branch) {
+			if (!$parsedBranch = $this->validateBranch($branch)) {
+				continue;
+			}
+
+			$data['version'] = $branch;
+			$data['version_normalized'] = $parsedBranch;
+
+			if ('dev-' === substr($parsedBranch, 0, 4) || '9999999-dev' === $parsedBranch) {
+				$data['version'] = 'dev-' . $data['version'];
+			} else {
+				$data['version'] = preg_replace('{(\.9{7})+}', '.x', $parsedBranch);
+			}
+
+			$versions[] = $data;
+		}
+
+		$stmt = $db->prepare('UPDATE packages SET last_fetched = datetime("now"), versions = :json WHERE name = :name AND type = :type');
+		$package->versions = json_encode($versions);
+		$stmt->execute(array(':name' => $package->name, ':json' => $package->versions, ':type' => $package->type));
+	}
+
+	private function validateBranch($branch) {
+		try {
+			return $this->versionParser->normalizeBranch($branch);
+		} catch (Exception $ex) {
+
+		}
+
+		return false;
+	}
+
+	private function validateTag($version) {
+		try {
+			return $this->versionParser->normalize($version);
+		} catch (Exception $ex) {
+
+		}
+
+		return false;
 	}
 
 }
